9ba81d199a9f7e35df1c632e2b39a32f4e56ff39
maksymprudnik <maksymprudnik@gmail.com>
2020-10-08 14:20:42 +0300

    changed format of data recieved from server

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Message.css';
 
 const Message = ({message, username}) => {
+    console.log(message)
     return <li className={`message-li message-li-${username === message.username && 'right'}`}>
         { username !== message.username && <span className='message-author'>{message.username}</span> }
-        <span className='message-body'>{message.message}</span>
-        <span className='message-date'>{message.createdAt}</span>
+        <span className='message-body'>{message.body}</span>
+        <span className='message-date'>{message.createdat}</span>
     </li>
 }
 
