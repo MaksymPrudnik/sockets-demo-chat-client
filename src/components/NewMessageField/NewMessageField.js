92e02e5983e4e5596a13b0f5adce82356b81a1e2
maksymprudnik <maksymprudnik@gmail.com>
2020-10-09 13:15:40 +0300

    added features - upload to s3 images

--- src/components/NewMessageField/NewMessageField.js
+++ src/components/NewMessageField/NewMessageField.js
@@ -2,33 +2,33 @@ import React, { useState } from 'react';
 import './NewMessageField.css';
 import { socket } from '../../service/socket';
 import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+
+const HOST = process.env.REACT_APP_HOST || 'http://localhost:5000';
 
 const NewMessageField = ({username}) => {
 
     const location = useLocation();
 
     const [message, setMessage] = useState('');
-    const [file, setFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
     const [error, setError] = useState('');
 
-    useEffect(() => {
-        socket.on('signed request send', (signedRequest, url) => {
-            fetch(signedRequest, {
-                method: 'put',
-                body: file
-            })
-            .then(response => {
-                if (!response.ok) return Promise.reject('Error uploading file')
-            })
-            .catch(err => setError(err))
+    const sendFileToS3 = (signedRequest, url, file) =>  {
+        fetch(signedRequest, {
+            method: 'put',
+            body: file
         })
-    }, [])
+        .then(response => {
+            if (!response.ok) return Promise.reject('Error uploading file')
+        })
+        .catch(err => setError(err.message || err.toString()))
+        .finally(() => setImageUrl(url))
+    }
 
     const handleChange = (e) => setMessage(e.target.value);
 
     const handleSend = () => {
-        socket.emit('new message', { channel: location.pathname.slice(1), body: {username, message}});
+        socket.emit('new message', { channel: location.pathname.slice(1), body: {username, message, img: imageUrl}});
         setMessage('')
     }
 
@@ -37,12 +37,18 @@ const NewMessageField = ({username}) => {
         if (!newFile) {
             return setError('No file selected');
         }
-        setFile(newFile);
-        socket.emit('get signed request', newFile.name, newFile.type);
+        fetch(`${HOST}/sign-s3?file-name=${newFile.name}&file-type=${newFile.type}`)
+        .then(response => {
+            if (!response.ok) return Promise.reject('Unable to get S3 URL')
+            return response.json()
+        })
+        .then(({signedRequest, url}) => sendFileToS3(signedRequest, url, newFile))
+        .catch(err => setError(err))
     };
 
     return location.pathname !== '/' && <section className='new-message-field-section'>
         {error && <p>{error}</p>}
+        {imageUrl && <img src={imageUrl} alt="uploaded content" style={{height: 100, width: 100}}/> }
         <input type="file" accept='image/png, image/jpeg' onChange={handleFileLoad}/>
         <input type='text' placeholder='Enter your message' value={message} onChange={handleChange}/>
         <button type='submit' onClick={handleSend}>Send</button>
